36484341094e23c1b8bf237751669f494979cbb2
Jameson54 <hehlol123123@mail.ru>
2025-09-26 15:57:20 +0700

    Peredelano LAB2.cpp

--- LAB2.cpp
+++ LAB2.cpp
@@ -9,32 +9,34 @@
 using namespace std;
 
 int main() {
-  double V, V0, P0;
-  const int k = 9;
-  int i;
-  double n[k], p;
-  
+  double V, V0, p0, p, n;
+
   cout << "V = ";
   cin  >> V;
-    
+
   cout << "V0 = ";
   cin  >> V0;
+
+  cout << "p0 = ";
+  cin  >> p0;
     
-  cout << "P0 = ";
-  cin  >> P0;
-  
-  cout << "Vvedite 9 eiementov:" << endl;
-  for (i = 0; i < 9; i++)
-  cin >> n[i];
+  cout << "n = ";
+  cin  >> n;
     
-  cout << setw(3) << "n" << setw(10) << "p" << endl;
-  cout << setw(25) << setfill('-') << "" << setfill(' ') << endl;
-  
-  cout  << fixed << setprecision(3);
-  for (i = 0; i < 9; i++) {  
-  p = P0 * pow(V / (V + V0), n[i]);
-  
-  cout << n[i] << "\t" << p << endl;
+  while (n < 50) {
+    p = p0 * pow((V / (V + V0)), n);
+    n = n + 10;
+
+    cout << "n = " << n << "\t \t" << p << endl;  
 }
-  return 0;
+
+  do {
+    n = n + 50;
+    p = p0 * pow((V / (V + V0)), n);
+  
+    cout << "n = " << n << "\t \t" << p << endl;
+  
+} while (n < 250);
+
+    return 0;
 }
